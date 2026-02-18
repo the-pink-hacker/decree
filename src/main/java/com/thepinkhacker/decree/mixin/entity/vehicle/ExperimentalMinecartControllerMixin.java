02ad6ede358d7b3a527c569bde9fff770142bd94
Pink Garrett <pink@thepinkhacker.com>
2026-02-17 22:38:39 -0500

    Fix minecart max speed gamerules
    The gamerule was returning as the max speed per tick instead of overall.

--- src/main/java/com/thepinkhacker/decree/mixin/entity/vehicle/ExperimentalMinecartControllerMixin.java
+++ src/main/java/com/thepinkhacker/decree/mixin/entity/vehicle/ExperimentalMinecartControllerMixin.java
@@ -2,37 +2,30 @@ package com.thepinkhacker.decree.mixin.entity.vehicle;
 
 import com.thepinkhacker.decree.world.DecreeGameRules;
 import net.minecraft.entity.vehicle.ExperimentalMinecartController;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.rule.GameRule;
+import net.minecraft.world.rule.GameRules;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ExperimentalMinecartController.class)
+@Debug(export = true)
 public abstract class ExperimentalMinecartControllerMixin {
-    @Inject(
-            method = "getMaxSpeed(Lnet/minecraft/server/world/ServerWorld;)D",
+    @Redirect(
+            method = "getMaxSpeed",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/rule/GameRules;getValue(Lnet/minecraft/world/rule/GameRule;)Ljava/lang/Object;"
-            ),
-            cancellable = true
+            )
     )
-    private void decreeGetMaxSpeed(ServerWorld world, CallbackInfoReturnable<Double> cir) {
+    private Object decree$GetMaxSpeed(GameRules instance, GameRule<Integer> rule) {
         boolean hasRider = ((ExperimentalMinecartController)(Object)this).minecart.hasPlayerRider();
 
-        if (hasRider) {
-            int speed = world.getGameRules().getValue(DecreeGameRules.MINECART_MAX_SPEED_RIDER);
+        int speed = instance.getValue(
+                hasRider ? DecreeGameRules.MINECART_MAX_SPEED_RIDER : DecreeGameRules.MINECART_MAX_SPEED_EMPTY
+        );
 
-            if (speed != 0) {
-                cir.setReturnValue((double) speed);
-            }
-        } else {
-            int speed = world.getGameRules().getValue(DecreeGameRules.MINECART_MAX_SPEED_EMPTY);
-
-            if (speed != 0) {
-                cir.setReturnValue((double) speed);
-            }
-        }
+        return speed == 0 ? instance.getValue(rule) : speed;
     }
 }
